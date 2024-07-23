23c8e5b480c22bb649f1f91dbad5ba7afbd797fd
Naif al-Sahabi <2210002362@iau.edu.sa>
2024-07-23 23:59:45 +0300

    2

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import uvicorn
+from starlette.requests import Request
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
 
 @app.get("/")
 async def name(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request, "name": "Nc4"})
+    return templates.TemplateResponse("home.html", {"request": request, "name": "Nc47"})
